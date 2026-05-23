189aeba2b6dbfae70b4173ca26da314d03b10ff4
RstK <208619198+bowman025@users.noreply.github.com>
2026-05-23 18:16:09 +0200

    Run format

--- apps/server/src/config/passport.js
+++ apps/server/src/config/passport.js
@@ -4,26 +4,23 @@ import bcrypt from 'bcryptjs';
 import { findUserByEmail } from '../services/userService.js';
 
 passport.use(
-  new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-      try {
-        const user = await findUserByEmail(email);
-        if (!user) {
-          return done(null, false, { message: 'Invalid credentials' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.passwordHash);
-        if (!isMatch) {
-          return done(null, false, { message: 'Invalid credentials' });
-        }
-
-        return done(null, user);
-      } catch (err) {
-        return done(err);
+  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await findUserByEmail(email);
+      if (!user) {
+        return done(null, false, { message: 'Invalid credentials' });
       }
+
+      const isMatch = await bcrypt.compare(password, user.passwordHash);
+      if (!isMatch) {
+        return done(null, false, { message: 'Invalid credentials' });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-  )
+  })
 );
 
 export default passport;
