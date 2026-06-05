c3a949931b83cb0a62115b0297b8161438e6c066
RstK <208619198+bowman025@users.noreply.github.com>
2026-06-05 20:42:57 +0200

    Edit style, update primary colors

--- apps/client/src/layouts/AuthLayout.jsx
+++ apps/client/src/layouts/AuthLayout.jsx
@@ -3,7 +3,20 @@ import { Outlet } from 'react-router';
 export default function AuthLayout() {
   return (
     <div className="auth-layout">
-      <Outlet />
+      <header className="auth-page-header">
+        <div className="auth-header-inner">Blabber</div>
+        <div className="auth-header-inner-sub">Keep on blabbing</div>
+      </header>
+
+      <main className="auth-page-main">
+        <Outlet />
+      </main>
+
+      <footer className="auth-page-footer">
+        <div className="auth-footer-inner">
+          Made by <a href="https://github.com/bowman025">bowman025</a>.
+        </div>
+      </footer>
     </div>
   );
 }
